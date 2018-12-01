e37eda3c23d69f8d2315d4c244c4b3fb34740e74
Yosaton <y.tungmanelatkul@gmail.com>
2018-12-01 18:24:52 +0800

    Navbar functional/Changed background/added logo

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  resources :user
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :user
+  resources :listings
 end
